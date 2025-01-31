a12559b1e65b88c5153c6ff4a475eb167077dc24
Andri Hadiansah <andrihadiansah@hotmail.com>
2025-01-31 23:09:23 +0700

    refactor: clean up blog page code, improve metadata generation, and enhance UI components

--- apps/portofolio/app/(app)/blog/[slug]/page.tsx
+++ apps/portofolio/app/(app)/blog/[slug]/page.tsx
@@ -8,14 +8,11 @@ import {
   PageHeaderDescription,
 } from "@workspace/ui/components/page-header";
 import Link from "next/link";
-import { buttonVariants } from "@workspace/ui/components/button";
+import { Button } from "@workspace/ui/components/button";
 import { ChevronLeft, UserPen } from "lucide-react";
 import { CustomMDX } from "@/components/mdx-components";
-import { cn } from "@workspace/ui/lib/utils";
 
-export const dynamicParams = false;
 export async function generateStaticParams() {
-<<<<<<< HEAD
   const posts = await getBlogPosts();
   return posts.map((post) => ({ slug: post.slug }));
 }
@@ -24,34 +21,19 @@ export async function generateMetadata({ params }: { params: any }) {
   const { slug } = await params;
   const post = getBlogPosts().find((post) => post.slug === slug);
 
-=======
-  let posts = getBlogPosts();
-
-  return posts.map((post) => ({
-    slug: post.slug,
-  }));
-}
-export async function generateMetadata({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) {
-  let post = getBlogPosts().find(
-    async (post) => post.slug === (await params).slug
-  );
->>>>>>> 7acec3681b60a771a7fb0d704bf4074d72ab3c85
   if (!post) {
-    notFound();
+    return null;
   }
-  let {
+
+  const {
     title,
     publishedAt: publishedTime,
     summary: description,
     image,
   } = post.metadata;
-  let ogImage = image
-    ? image
-    : `${siteConfig}/og?title=${encodeURIComponent(title)}`;
+  const ogImage = image
+    ? `${siteConfig.url}${image}`
+    : `${siteConfig.url}/og?title=${encodeURIComponent(title)}`;
 
   return {
     title,
@@ -60,19 +42,9 @@ export async function generateMetadata({
       title,
       description,
       type: "article",
-<<<<<<< HEAD
       publishedTime: formatDate(publishedTime, true),
       url: `${siteConfig.url}/blog/${post.slug}`,
       images: [{ url: ogImage }],
-=======
-      publishedTime,
-      url: `${siteConfig}/blog/${post.slug}`,
-      images: [
-        {
-          url: ogImage,
-        },
-      ],
->>>>>>> 7acec3681b60a771a7fb0d704bf4074d72ab3c85
     },
     twitter: {
       card: "summary_large_image",
@@ -83,20 +55,9 @@ export async function generateMetadata({
   };
 }
 
-<<<<<<< HEAD
 export default async function MDXPage({ params }: { params: any }) {
   const { slug } = await params;
   const post = getBlogPosts().find((post) => post.slug === slug);
-=======
-export default async function MDXPage({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) {
-  let post = getBlogPosts().find(
-    async (post) => post.slug === (await params).slug
-  );
->>>>>>> 7acec3681b60a771a7fb0d704bf4074d72ab3c85
 
   if (!post) {
     notFound();
@@ -119,7 +80,7 @@ export default async function MDXPage({
               image: post.metadata.image
                 ? `${siteConfig.url}${post.metadata.image}`
                 : `/og?title=${encodeURIComponent(post.metadata.title)}`,
-              url: `${siteConfig.url}/blog/${post.slug}`,
+              url: `${siteConfig.url}/thoughts/${post.slug}`,
               author: {
                 type: "Person",
                 name: siteConfig.name,
@@ -128,31 +89,28 @@ export default async function MDXPage({
           }}
         />
 
-        <PageHeader>
-          <PageHeaderHeading>{post.metadata.title}</PageHeaderHeading>
+        <PageHeader className="">
+          <PageHeaderHeading className="">
+            {post.metadata.title}
+          </PageHeaderHeading>
           <PageHeaderDescription>{post.metadata.summary}</PageHeaderDescription>
           <PageActions className="flex gap-2">
             {formatDate(post.metadata.publishedAt, true)}
-            <Link href={"/about"}>about</Link>
+            <Link href={"/about"}>
+              <Button variant={"outline"} withIcon={<UserPen />} size={"sm"}>
+                Author
+              </Button>
+            </Link>
           </PageActions>
         </PageHeader>
         <article className="  container-wrapper p-4 px-16 max-md:px-8 ">
           <CustomMDX source={post.content} />
         </article>
         <div className="container-wrapper p-4 max-md:px-4 px-8">
-<<<<<<< HEAD
           <Link href={"/blog"}>
             <Button variant={"outline"} withIcon={<ChevronLeft />}>
               All Posts
             </Button>
-=======
-          <Link
-            href={"/blog"}
-            className={cn(buttonVariants({ variant: "secondary" }))}
-          >
-            <ChevronLeft />
-            All Posts
->>>>>>> 7acec3681b60a771a7fb0d704bf4074d72ab3c85
           </Link>
         </div>
       </section>
